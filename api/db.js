fcc82cb2177fcaf403a719a1adf1794e6f959fb5
juanhuarte <juan_huarte@hotmail.com>
2022-05-05 18:59:29 -0300

    update db file

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGO_DB_URI; // string de conexion
+const MONGODB_URI = process.env.MONGO_DB_URI;
 
 mongoose
   .connect(MONGODB_URI, {
